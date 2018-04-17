a3a5645b626b5cbda7eed4f6e682276c3eea4b2b
zweizhao <zweizhao@foxmail.com>
2018-04-17 11:11:18 +0800

    打包

--- src/route/core/index.js
+++ src/route/core/index.js
@@ -4,5 +4,4 @@ export default [
 	{ path: 'inventory', component: () => import('core/inventory/index.vue') },
 	{ path: 'more', component: () => import('core/more/index.vue') },
 	{ path: 'procurement', component: () => import('core/procurement/index.vue') },
-	{ path: 'statistics', component: () => import('core/statistics/index.vue') },
 ]
